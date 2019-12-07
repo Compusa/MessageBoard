b5d1da1915075e1b0c2b4a6a10afbe6fac40c613
Joel Hutters <joel.hutters@p-bolaget.goteborg.se>
2019-12-07 23:06:11 +0100

    Scaffolded projects

---
+++ src/MessageBoard.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MessageBoard.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/MessageBoard.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MessageBoard.Domain
+{
+    public class Class1
+    {
+    }
+}
