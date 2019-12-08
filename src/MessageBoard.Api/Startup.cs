d56ed698d2fd860acf5a91a1ae60bb1eb26a4674
Joel Hutters <joel.hutters@p-bolaget.goteborg.se>
2019-12-08 16:34:20 +0100

    Added Mediatr

--- src/MessageBoard.Api/Startup.cs
+++ src/MessageBoard.Api/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MediatR;
+using MessageBoard.Application;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace MessageBoard.Api
 {
@@ -26,6 +21,7 @@ namespace MessageBoard.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddMediatR(typeof(MessageDto).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
