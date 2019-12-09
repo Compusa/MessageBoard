7cfd4cfad203645a176aaade6c0aa27d8940cdba
Joel Hutters <joel.hutters@p-bolaget.goteborg.se>
2019-12-09 18:41:15 +0100

    Added find message by id endpoint (draft)

---
+++ src/MessageBoard.Api/Controllers/MessagesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using MediatR;
+using MessageBoard.Application;
+using MessageBoard.Application.Messages.Queries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MessageBoard.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public MessagesController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        /// <summary>
+        /// Find message by id.
+        /// </summary>
+        /// <response code="200">The message with the specified id.</response>
+        /// <response code="404">If message with specified id doesn't exist</response>
+        [HttpGet("{id}", Name = "Get")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<MessageDto>> Get(int id)
+        {
+            var message = await _mediator.Send(new GetMessageQuery(id));
+
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(message);
+        }
+    }
+}
--- src/MessageBoard.Application/Messages/MessageDto.cs
+++ src/MessageBoard.Application/Messages/MessageDto.cs
@@ -4,5 +4,8 @@ namespace MessageBoard.Application
 {
     public class MessageDto
     {
+        public int Id { get; set; }
+
+        public int ClientId { get; set; }
     }
 }
