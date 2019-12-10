041fd5ef92b6209d83cbd5aa6bdd54cb7607076b
Joel Hutters <joel.hutters@p-bolaget.goteborg.se>
2019-12-11 00:30:54 +0100

    Added required Domain SeedWork files

---
+++ src/MessageBoard.Domain/SeedWork/Entity.cs
@@ -0,0 +1,75 @@
+﻿namespace MessageBoard.Domain.SeedWork
+{
+    public abstract class Entity
+    {
+        private int? _requestedHashCode;
+
+        public virtual int Id { get; protected set; }
+
+        public bool IsTransient()
+        {
+            return Id == default;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Entity))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Entity item = (Entity)obj;
+
+            if (item.IsTransient() || IsTransient())
+            {
+                return false;
+            }
+            else
+            {
+                return item.Id == Id;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            if (IsTransient())
+            {
+                return base.GetHashCode();
+            }
+
+            if (!_requestedHashCode.HasValue)
+            {
+                _requestedHashCode = Id.GetHashCode() ^ 31;
+            }
+            // XOR for random distribution. See:
+            // https://blogs.msdn.microsoft.com/ericlippert/2011/02/28/guidelines-and-rules-for-gethashcode/
+
+            return _requestedHashCode.Value;
+        }
+
+        public static bool operator ==(Entity left, Entity right)
+        {
+            if (Equals(left, null))
+            {
+                return Equals(right, null);
+            }
+            else
+                return left.Equals(right);
+        }
+
+        public static bool operator !=(Entity left, Entity right)
+        {
+            return !(left == right);
+        }
+    }
+}
